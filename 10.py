40eee1257861c650636ef62f4182d75dee905c36
Kayki-Borges <144156841+Kayki-Borges@users.noreply.github.com>
2023-11-13 16:51:31 -0300

    Lista 2

--- 10.py
+++ 10.py
@@ -1,13 +1,17 @@
 s = float(input("Digite o salário:"))
+print("-"*20)
 if s <= 280:
-    print("Você teve um aumento de 20%, seu slário agora é {}R$".format((20*s)/100))
+    print("Você teve um aumento de 20%, seu salário agora é {}R$".format(s*(20/100)+s))
 elif s > 280 and s < 700:
-    print("Você teve um aumento de 15%, seu salário agora é {}R$".format((15*s)/100))
+    print("Você teve um aumento de 15%, seu salário agora é {}R$".format(s*(15/100)+s))
 elif s > 700 and s < 1500:
-    print("Você teve um aumento de 10%, seu salário agora é {}R$".format((10*s)/100))
+    print("Você teve um aumento de 10%, seu salário agora é {}R$".format(s*(10/100)+s))
 else :
     print("Seu salário é de {}R$".format(s))
-    print("O percentual de aumento aplicado é de {}".format("10%"))
-    print("Seu salário vai aumentar no valor de {}R$".format("100"))
-    print("O npvp salário é {}".format((5*s)/100))
+    print("-"*20)
+    print("O percentual de aumento aplicado é de 5%")
+    print("-"*20)
+    print("Seu salário vai aumentar no valor de {}R$".format((s*(5/100)+s)-s))
+    print("-"*20)
+    print("O novo salário é {}".format(s*(5/100)+s))
 
--- 11.py
+++ 11.py
@@ -3,9 +3,9 @@ h = float(input("Digite a quantidade do horas trabalhadas:"))
 sb = s*h
 print("Salário bruto:{}".format(sb))
 if sb >= 900 and sb <= 1500:
-    print("Ir:{}:{}".format("5%", 5/100))
+    print("Ir:{}:{}".format("5%", s*(5/100)+s))
 elif sb > 1500 and sb <= 2500:
-    print("Ir:{}:{}".format("10%",10/100))
+    print("Ir:{}:{}".format("10%",s*(10/100)+s))
 else :
-    print("Ir:{}:{}".format("20%",20/100))
+    print("Ir:{}:{}".format("20%",s*(20/100)+s))
 print("FGTS:{}".format("10%"))
