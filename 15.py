95080a0b0fab56b7db85114fd08847bcaabacc42
Kayki-Borges <144156841+Kayki-Borges@users.noreply.github.com>
2023-11-21 15:23:19 -0300

    Lista 2

--- 15.py
+++ 15.py
@@ -1,5 +1,17 @@
-a = float(input("Digite o valor de A"))
-if a <= 0:
-    print("A e igual a 0, logo não é uma equação do segundo grau,o pragrama não será continuado!")
-b = float(input("Digite o valor de B"))
-c = float(input("Digite o valor de C"))
+a = float(input("Digite o valor de a: "))
+if a == 0:
+    print("isto não é uma equação do segundo grau!")
+else:
+    b = float(input("Digite o valor de b:"))
+    c = float(input("Digite o valor de c:"))
+    delta = (b ** 2) - (4 * a * c)
+    if delta < 0:
+        print("Resultado de delta é menor que 0. Não há raízes reais.")
+    elif delta == 0:
+        raiz1 = (-b) / (2 * a)
+        print("Delta é zero. A raíz é {}".format(raiz1))
+    else:
+        raiz2 = (-b + (delta ** (1 / 2))) / (2 * a)
+        raiz3 = (-b - (delta ** (1 / 2))) / (2 * a)
+        print(
+            f"Delta maior que zero. A raíz 1 é {raiz2}, e a raiz 2 é {raiz3}")
---
+++ 17.py
@@ -0,0 +1,7 @@
+ano = int(input("Digite o ano:"))
+mes = int(input("Digite o mês:"))
+dia = int(input("Digite o dia:"))
+if ano < 0:
+    print("O ano é inválido!")
+else:
+    print("Ano é válido!")
